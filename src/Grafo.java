94353f6cbf8e4173c064afe7b83e7c12a1adad24
gabriellm.dev <gabriellm.dev@gmail.com>
2024-06-19 10:17:00 -0300

    Atualizacao do grafo para permitir colaboracao entre autores

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Grafo {
     private Map<Autor, Set<Autor>> adjList;
@@ -10,19 +7,24 @@ public class Grafo {
         this.adjList = new HashMap<>();
     }
 
-    public void adicionarAutor(Autor autor){
+    public void adicionarAutor(Autor autor) {
         adjList.putIfAbsent(autor, new HashSet<>());
     }
 
     public void adicionarColaboracao(Autor autor1, Autor autor2) {
-        adjList.putIfAbsent(autor1, new HashSet<>());
-        adjList.putIfAbsent(autor2, new HashSet<>());
-        adjList.get(autor1).add(autor2);
-        adjList.get(autor2).add(autor1);
+        if (adjList.containsKey(autor1) && adjList.containsKey(autor2)) {
+            adjList.get(autor1).add(autor2);
+            adjList.get(autor2).add(autor1);
+        } else {
+            System.out.println("Um ou ambos os autores não existem.");
+        }
+    }
+
+    public Set<Autor> getAutores() {
+        return adjList.keySet();
     }
 
     public Map<Autor, Set<Autor>> getAdjList() {
         return adjList;
     }
-
 }
